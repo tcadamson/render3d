3851998574fb251c373405f8a11516b8db93ce6e
tcadamson <taylor@catcat.dev>
2022-12-05 05:46:23 -0700

    Add cmake rules for glfw + opengl, window demo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-    std::cout<<"Hello, World!"<<std::endl;
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+    window = glfwCreateWindow(640, 480, "render3d", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glClearColor( 0.f, 0.f, 0.f, 0.f);
+    while (!glfwWindowShouldClose(window))
+    {
+        // Render here
+        glClear(GL_COLOR_BUFFER_BIT);
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+        // Poll for and process events
+        glfwPollEvents();
+    }
+    glfwTerminate();
+
     return 0;
 }
