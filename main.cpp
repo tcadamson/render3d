67e4fbe525f784e37df2d87acb27b085d2dee202
tcadamson <taylor@catcat.dev>
2022-12-06 01:07:25 -0700

    Add triangle render test

--- main.cpp
+++ main.cpp
@@ -1,19 +1,53 @@
 #include <iostream>
+#include <vector>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-void framebufferSizeCallback(GLFWwindow *window, int width, int height)
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
+const int OPENGL_VERSION_MAJOR = 3;
+const int OPENGL_VERSION_MINOR = 3;
+
+void framebufferSizeCallback(GLFWwindow */*window*/, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
 
+unsigned int compileShader(int shaderType, const char *shaderSource)
+{
+    unsigned int shader = glCreateShader(shaderType);
+    int success;
+    int logLength;
+    glShaderSource(shader, 1, &shaderSource, nullptr);
+    glCompileShader(shader);
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLength);
+    if (!success)
+    {
+        char log[logLength];
+        glGetShaderInfoLog(shader, logLength, nullptr, log);
+        std::cout << "Shader " << shaderType << " compilation failed\n" << log << std::endl;
+    }
+    return shader;
+}
+
+unsigned int createShaderProgram(const std::vector<unsigned int> &shaders)
+{
+    unsigned int shaderProgram = glCreateProgram();
+    for (unsigned int shader : shaders)
+    {
+        glAttachShader(shaderProgram, shader);
+    }
+    glLinkProgram(shaderProgram);
+    for (unsigned int shader : shaders)
+    {
+        glDeleteShader(shader);
+    }
+    return shaderProgram;
+}
+
 int main()
 {
-    const int WINDOW_WIDTH = 1280;
-    const int WINDOW_HEIGHT = 720;
-    const int OPENGL_VERSION_MAJOR = 3;
-    const int OPENGL_VERSION_MINOR = 3;
-
     // GLFW initialization and window setup (supports OpenGL >= 3.3)
     if (!glfwInit())
         return -1;
@@ -23,7 +57,7 @@ int main()
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "render3d", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "render3d", nullptr, nullptr);
     if (!window)
     {
         std::cout << "Failed to initialize GLFW window" << std::endl;
@@ -42,16 +76,58 @@ int main()
         return -1;
     }
 
+    // Triangle render test
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+    const char *vertexShaderSource =
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 pos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(pos.x, pos.y, pos.z, 1.f);\n"
+        "}\0";
+    const char *fragmentShaderSource =
+        "#version 330 core\n"
+        "out vec4 color;\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.f, 1.f, 1.f, 1.f);\n"
+        "}\0";
+    std::vector<unsigned int> shaders = {
+        compileShader(GL_VERTEX_SHADER, vertexShaderSource),
+        compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource)
+    };
+    unsigned int vao;
+    unsigned int vbo;
+    unsigned int shaderProgram = createShaderProgram(shaders);
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)nullptr);
+    glEnableVertexAttribArray(0); // 0 is the vertex attribute location as defined in vertex shader
+
     while (!glfwWindowShouldClose(window))
     {
-        // Input
         // Render
         glClearColor(0, 0, 0, 0);
         glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    // Clean up OpenGL and GLFW resources
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteProgram(shaderProgram);
     glfwTerminate();
 
     return 0;
