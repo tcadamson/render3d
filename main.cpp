8d03b389523c68a9e3439c6b25a49fa20f1dad75
tcadamson <taylor@catcat.dev>
2022-12-05 20:22:37 -0700

    Center window on primary monitor

--- main.cpp
+++ main.cpp
@@ -14,22 +14,26 @@ int main()
     const int OPENGL_VERSION_MAJOR = 3;
     const int OPENGL_VERSION_MINOR = 3;
 
-    // GLFW initialization (supports OpenGL >= 3.3)
+    // GLFW initialization and window setup (supports OpenGL >= 3.3)
     if (!glfwInit())
         return -1;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, OPENGL_VERSION_MAJOR);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, OPENGL_VERSION_MINOR);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Needed for OS X compatibility
-    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "render3d", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "render3d", NULL, NULL);
     if (!window)
     {
         std::cout << "Failed to initialize GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-    glfwMakeContextCurrent(window);
+    const GLFWvidmode *mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    glfwSetWindowPos(window, (mode->width - WINDOW_WIDTH) / 2, (mode->height - WINDOW_HEIGHT) / 2); // Center window on primary monitor
     glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+    glfwMakeContextCurrent(window);
+
+    // GLAD initialization (must be called before any OpenGL function)
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
@@ -44,7 +48,7 @@ int main()
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
     glfwTerminate();
+
     return 0;
 }
