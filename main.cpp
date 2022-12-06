ddd6acdb14cbfd7509b0ce1963bed97409655b67
tcadamson <taylor@catcat.dev>
2022-12-05 18:14:05 -0700

    Add opengl version hints and a window resize handler

--- main.cpp
+++ main.cpp
@@ -2,25 +2,37 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void framebufferSizeCallback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
 int main()
 {
-    GLFWwindow* window;
-
+    // GLFW initialization (supports OpenGL >= 3.3)
     if (!glfwInit())
         return -1;
-    window = glfwCreateWindow(640, 480, "render3d", NULL, NULL);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Needed for OS X compatibility
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "render3d", NULL, NULL);
     if (!window)
     {
+        std::cout << "Failed to initialize GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    glClearColor( 0.f, 0.f, 0.f, 0.f);
+
+    glViewport(0, 0, 1280, 720);
+    glClearColor(0, 0, 0, 0);
     while (!glfwWindowShouldClose(window))
     {
         // Render here
